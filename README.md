9d0b3fd3b0a1e7c4ee5714e01d3a99c2427c8e75
Paul Antropius <paul.antropius@kstor.net>
2022-02-14 22:47:26 +0100

    Change initial location.

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ This repo is used as the front (React) part of the [peaks app](https://github.co
 After importing the code and from within the root project directory, run:
 
 ```sh
+$ cd mfi-front # or front, according to the context.
 $ npm install
 $ npm start
 ```
+Visit your browser at http://localhost:3000/.
--- src/Map.js
+++ src/Map.js
@@ -161,7 +161,7 @@ const Map = () => {
   }
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} className="map"
+    <MapContainer center={[45.5, -3]} zoom={6} className="map"
     MapPlaceholder={<MapPlaceholder />} whenCreated={init}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
