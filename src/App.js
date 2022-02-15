2c2cdcdd84a8ac96a0f288285951dd969a28a6cc
Paul Antropius <paul.antropius@kstor.net>
2022-02-15 08:56:44 +0100

    Add error boundary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import './App.css';
-
+import { Component } from 'react';
 import Map from './Map';
+
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Mettez à jour l'état, de façon à montrer l'UI de repli au prochain rendu.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Vous pouvez aussi enregistrer l'erreur au sein d'un service de rapport.
+    // logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Vous pouvez afficher n'importe quelle UI de repli.
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
 
 
 function App() {
 
-  return <Map />
+  return (
+    <ErrorBoundary>
+      <Map />
+    </ErrorBoundary>
+  )
 }
 
 export default App;
